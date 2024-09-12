984da843d59c4e9e0603cdb0f6025289be55ebf4
dniketh <dniketh@actalentservices.com>
2024-09-12 19:11:02 +0000

    CSharp basics

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public string Name = "Niketh";
+        protected string PhoneNumber = "8247373288";
+        private double _salary = 44000.00;
+        protected internal string Address = "Estay Beta PG";
+        private protected float Balance = 450F;
+    }
+}
---
+++ Oops/Program.cs
@@ -0,0 +1,13 @@
+﻿using ClassLibrary1;
+namespace Oops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+        }
+        
+    }
+}
